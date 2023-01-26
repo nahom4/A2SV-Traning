64a012876c48ba8a005340052b8efd98bfea733c
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-01-26 19:36:17 +0300

    Time: 39 ms (66.41%), Space: 13.9 MB (67.65%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,41 @@
+class Solution:
+    
+    def swap(self,i,j,matrix,temp):
+        
+      
+       
+        n = len(matrix[0])-1
+        
+        
+        value = matrix[j][n-i]
+        matrix[j][n-i] = temp
+        return [(j,n-i),value]
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        #we can use transformations to solve the problem
+        checked = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if not (i,j) in checked:
+                    
+                   
+                    temp = matrix[i][j]
+                    first = i
+                    second = j
+                 
+                    for _ in range(4):
+                        nxt = self.swap(first,second,matrix,temp)
+                        checked.add((first,second))
+                        first,second = nxt[0]
+                        temp = nxt[1]
+                        
+                  
+                   
+                   
+               
+                
+            
+        
