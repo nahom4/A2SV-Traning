ba0b148a52b29a42aa943f6ddcf554ab4f90874d
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-03-22 16:50:25 +0300

    Time: 1875 ms (81.03%), Space: 106.3 MB (60.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -3,24 +3,42 @@ class Solution:
         
         dic = defaultdict(list)
         visited = set()
+        tree_queue = deque()
+        tree_queue.append(source)
         for edge in edges:
             first = edge[0]
             second = edge[1]
             dic[first].append(second)
             dic[second].append(first)
-         
-        def dfs(n,source,destination):
-            visited.add(source)
-            if source == destination:
+            
+          # bfs solution
+        while  tree_queue:
+            
+            val = tree_queue.popleft()
+            if val == destination:
                 return True
+            for neighbor in dic[val]:
+                if neighbor not in visited:
+                    tree_queue.append(neighbor)
+                    visited.add(neighbor)
+        return False
+
             
-            for neighbor in dic[source]:
+# dfs
+        #def dfs(n,source,destination):
+#             visited.add(source)
+#             if source == destination:
+#                 return True
+            
+#             for neighbor in dic[source]:
                 
-                if neighbor not in visited and dfs(n,neighbor,destination):
-                    return True
+#                 if neighbor not in visited and dfs(n,neighbor,destination):
+#                     return True
             
-            return False
+#             return False
         
-        return dfs(n,source,destination)
-            
+#         return dfs(n,source,destination)
+
+       
+      
         
