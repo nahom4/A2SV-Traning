8e2470dd52c1be6480df3af65082d82c33b1d89a
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-05-17 18:21:12 +0300

    Time: 2083 ms (49.17%), Space: 108.7 MB (52.99%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,43 +1,50 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        dic = defaultdict(list)
-        visited = set()
-        tree_queue = deque()
-        tree_queue.append(source)
-        for edge in edges:
-            first = edge[0]
-            second = edge[1]
-            dic[first].append(second)
-            dic[second].append(first)
-            
-          # bfs solution
-        while  tree_queue:
-            
-            val = tree_queue.popleft()
-            if val == destination:
-                return True
-            for neighbor in dic[val]:
-                if neighbor not in visited:
-                    tree_queue.append(neighbor)
-                    visited.add(neighbor)
-        return False
-
-            
-# dfs
-        #def dfs(n,source,destination):
-#             visited.add(source)
-#             if source == destination:
-#                 return True
-            
-#             for neighbor in dic[source]:
-                
-#                 if neighbor not in visited and dfs(n,neighbor,destination):
-#                     return True
-            
-#             return False
+        rep = {i : i for i in range(n)}
+        store = [1] * n
         
-#         return dfs(n,source,destination)
+        # use quick union
+        def findRep(node):
+            parent = node
+            while parent != rep[parent]:
+                
+                parent = rep[parent]
+                
+            #let's do here path comprehension
+            while node != rep[node]:
+                oldParent = rep[node]
+                rep[node] = parent
+                node = oldParent
+            
+            return parent
+        
+        def union(first, second):
+            
+            repFirst = findRep(first)
+
+            repSecond = findRep(second)
+              
+            lnFirst = store[repFirst]
+            lnSecond = store[repSecond]
+            
+            if lnFirst > lnSecond:
+                rep[repSecond] = repFirst
+                store[repFirst] += store[repSecond]
+                
+            else:
+                rep[repFirst] = repSecond
+                store[repSecond] += store[repFirst]
+                       
+        #build the grouping
+        for first,second in edges:
+            union(first,second)
+            
+        return findRep(source) == findRep(destination)
+                
+            
+        
+       
 
        
       
