4ab6ab0c95993bb5270f778cf62a363726ed7d57
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-05-26 18:38:28 +0300

    Time: 52 ms (8.32%), Space: 16.4 MB (8.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        lis = [-1] * 2*n
+        lis = [-1] *n
         def climbStairs(n):
-            print(n)
-            print(lis)
-            if lis[n - 1] != -1:
-                return lis[n - 1]
             if n < 0:
                 return 0
             
             if n == 0:
                 return 1
+         
+            if lis[n - 1] != -1:
+                return lis[n - 1]
             
             lis[n - 1] = climbStairs(n - 1) + climbStairs(n - 2)
             
