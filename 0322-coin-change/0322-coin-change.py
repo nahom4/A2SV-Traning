126395cfc2c9bee62f8549a804d8c1982e51f28a
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-07-09 16:12:15 +0400

    Time: 2109 ms (5.61%), Space: 14.9 MB (21.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,24 +5,42 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        cache = {}
-        def dp(sm):
+#         cache = {}
+#         def dp(sm):
             
-            if sm == amount:
-                return 0
+#             if sm == amount:
+#                 return 0
             
-            if sm > amount:
-                return float('inf')
+#             if sm > amount:
+#                 return float('inf')
             
-            if sm in cache:
-                return cache[sm]
-            min_num_coins = float('inf')
+#             if sm in cache:
+#                 return cache[sm]
+#             min_num_coins = float('inf')
+#             for coin in coins:
+#                 min_num_coins = min(1 + dp(sm + coin),min_num_coins)
+                
+#             cache[sm] = min_num_coins
+                
+#             return min_num_coins
+#         result = dp(0)
+#         return -1 if result == float('inf') else result
+
+        cache = {0:0}
+        
+        for sm in range(1,amount + 1):
+            cache[sm] = float('inf')
             for coin in coins:
-                min_num_coins = min(1 + dp(sm + coin),min_num_coins)
+                num_coin = cache.get(sm - coin,float('inf'))
+               
+                num_coin += 1
+                cache[sm] = min(cache[sm],num_coin)
+                 
+        return -1 if cache[amount] == float('inf') else cache[amount]
+                    
                 
-            cache[sm] = min_num_coins
                 
-            return min_num_coins
-        result = dp(0)
-        return -1 if result == float('inf') else result
+                     
+      
+
         
