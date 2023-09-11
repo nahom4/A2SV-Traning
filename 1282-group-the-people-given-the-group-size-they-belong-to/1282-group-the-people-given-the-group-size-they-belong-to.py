65299739b2e76eddad23987154c3e867229e2478
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-09-11 13:45:14 +0300

    Time: 70 ms (82.92%), Space: 16.5 MB (31.58%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,26 +1,27 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        #let's use double for loop
-        ans = [[-1]]
+        
+        #let's optimize using a dictionary
+        cache = defaultdict(list)
+        ans = []
         for index,size in enumerate(groupSizes):
-            found = False
-            for group in ans:
-                if group[0] == size and len(group) - 1 < size:
-                    found = True
-                    group.append(index)
-                    break
-            if not found:
-                newGroup = [size,index]
-                ans.append(newGroup)
+            if cache[size]:
+                if len(cache[size]) < size:
+                    cache[size].append(index)
+                    
+                else:
+                    ans.append(cache[size])
+                    cache[size] = [index]
+            else:
+                cache[size] = [index]
                 
-        newAns = []
                 
-        for group in ans:
-            if group[0] != -1:
-                newAns.append(group[1 : ])
-            
-            
-        return newAns
+        for key in cache:
+            if cache[key]:
+                ans.append(cache[key])
+                     
+        return ans
+                
                 
         
         
