1d1d9a706a0fbf2579db41935b5782f1b6a1b728
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-09-14 14:59:44 +0300

    Time: 88 ms (64.27%), Space: 16.8 MB (87.47%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,21 +1,42 @@
-from collections import defaultdict
-
 class Solution:
-    def findItinerary(self, tickets):
-        # Build a graph where the edges are sorted lexicographically
-        graph = defaultdict(list)
-        for ticket in sorted(tickets, reverse=True):
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        """
+         let's do a kind of dfs whenever a person takes a flight we remove it                   connetion with that city whatever result the cities return we take the
+         let's build a directed graph
+        """
+        tickets.sort()
+        graph = defaultdict(deque)
+        for ticket in tickets:
             graph[ticket[0]].append(ticket[1])
-        
-        def dfs(node):
-            # Visit all destinations from the current airport
-            while graph[node]:
-                next_dest = graph[node].pop()
-                dfs(next_dest)
-            # Add the current airport to the itinerary
-            result.append(node)
-        
-        result = []
-        dfs("JFK")
-        # Reverse the result to get the correct itinerary
-        return result[::-1]
+            
+        N = len(tickets)
+        cities = []
+        def dfs(city,noTickets):
+            
+            if noTickets != N and not graph[city]:
+                return False
+            if noTickets == N and not graph[city]:
+                return True
+            
+            itenery = None
+            count = len(graph[city])
+
+            while count > 0:
+                child = graph[city].popleft()
+                res = dfs(child,noTickets + 1)    
+                cities.append(child)
+                if res:
+                    return res
+                graph[city].append(child)
+                cities.pop()
+                count -= 1
+                
+            return False
+                
+        dfs('JFK',0)
+        return ["JFK"] + cities[:: -1 ]
+                
+                
+                
+                
