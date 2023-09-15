99a9c4a46d1eb2000dd432d9ad9e70e35144c1e0
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-09-15 10:59:20 +0300

    Time: 805 ms (91.48%), Space: 81.3 MB (81.63%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,55 +1,29 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        points = list(map(tuple,points))
-        rep = {point : point for point in points}
-        rank = {point : 1 for point in points}
         
-        def find(node):
-            
-            if node == rep[node]:
-                return node
-            
-            parent = find(rep[node])
-            rep[node] = parent
-            return parent
+        #let's try prims algorithm
+        visited = set()
+        heap = [(0,0, 0)]
+        N = len(points)
         
-        
-        def union(firstNode,secondNode):
-            firstNodeParent,secondNodeParent = find(firstNode),find(secondNode)
-            
-            if firstNodeParent == secondNodeParent:
-                return False
-            
-            if rank[firstNodeParent] < rank[secondNodeParent]:
-                firstNodeParent,secondNodeParent = secondNodeParent,firstNodeParent
-                
-            rep[secondNodeParent] = firstNodeParent
-            rank[firstNodeParent] += rank[secondNodeParent]
-            return True
-            
-        def manhattanDistance(pointA,pointB):
+        def  manhattanDistance(pointA,pointB):
             xA,yA = pointA
             xB,yB = pointB
             
             return abs(xA - xB) + abs(yA - yB)
-            
-        pairs = []
-        N = len(points)
-        for i in range(N):
-            for j in range(N):
-                if i == j: 
-                    continue
-                pairs.append((manhattanDistance(points[i],points[j]),points[i],points[j]))
-                
-        pairs.sort()
+        
         minCost = 0
-        for distance,pairA,pairB in pairs:
-            if union(pairA,pairB):
+        while len(visited) < N:
+            distance,pointA,pointB = heapq.heappop(heap)
+            
+            if not pointB in visited:
                 minCost += distance
-                
-                
+                visited.add(pointB)
+                for i in range(N):
+                    if not i in visited:
+                                heapq.heappush(heap,(manhattanDistance(points[pointB],points[i]),pointA,i))
+
         return minCost
-                
             
-            
+    
         
