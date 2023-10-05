7bfe5acf9af19b4b00dd5414c8c5fcdd649878ed
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-05 17:02:30 +0300

    Time: 3532 ms (28.00%), Space: 16.9 MB (35.19%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -10,20 +10,33 @@ class Solution:
             if root ** 2 == num:
                 array.append(num)
            
-        @cache
-        def dp(sm): #[1,4,9]
-            if sm > n:
-                return float("inf")
+        # @cache
+#         def dp(sm): #[1,4,9]
+#             if sm > n:
+#                 return float("inf")
             
-            if sm == n:
-                return 0
-            minValue = float("inf")
-            for value in array:
-                minValue = min(dp(value + sm),minValue)
+#             if sm == n:
+#                 return 0
+#             minValue = float("inf")
+#             for value in array:
+#                 minValue = min(dp(value + sm),minValue)
                 
-            return minValue + 1
+#             return minValue + 1
         
-        return dp(0)
+#         return dp(0)
+
+    
+        cache = {0 : 0} # [1,4,9]
+        for num in range(1,n + 1):
+            minSize = float("inf")
+            for value in array:
+                if num - value in cache:
+                    minSize = min(minSize,cache[num - value] + 1) 
+                    
+            cache[num] = minSize
+      
+        return cache[n]
+                
                 
       
         
