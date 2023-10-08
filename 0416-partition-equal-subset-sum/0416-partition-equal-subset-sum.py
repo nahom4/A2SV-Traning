f635054a451f9e4dd58ce2f17dd95bbc982e7cb3
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-08 17:00:24 +0300

    Time: 4697 ms (5.00%), Space: 48.1 MB (27.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,17 +4,35 @@ class Solution:
         
         if total % 2 != 0:
             return False
-        target = total // 2
+        target = total
         N = len(nums)
-        @cache
-        def dp(i,sm):
-            if i >= N:
-                if sm == target:
-                    return True
-                return False
+#         @cache
+#         def dp(i,sm):
+#             if i >= N:
+#                 if sm == target:
+#                     return True
+#                 return False
             
-            return dp(i + 1,sm + nums[i]) or dp(i + 1,sm)
+#             return dp(i + 1,sm + nums[i]) or dp(i + 1,sm)
         
         
-        return dp(0,0)
+#         return dp(0,0)
+
+        
+        cache = [[False] * (target + 1) for _ in range(N)]
+        
+       
+        for i in range(N):
+            cache[i][0] = True
+      
+        cache[0][nums[0]] = True
+        
+        for i in range(1,N):
+            for sm in range(1,target + 1):
+                leave = False
+                if sm - nums[i] >= 0:
+                    leave = cache[i - 1][sm - nums[i]]
+                cache[i][sm] = cache[i - 1][sm] or leave
+                        
+        return cache[N - 1][target // 2]
                 
