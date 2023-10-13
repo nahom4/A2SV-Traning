ca41c2ce64966425bed210cc4f35a54945bf7e8d
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-13 18:51:10 +0300

    Time: 49 ms (14.52%), Space: 16.3 MB (67.43%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,25 +1,14 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        cache = [0] * (n + 1)
-        multi = [0] * (n + 1)
-        if n == 1:
-            return 1
-        
-        if n == 2:
-            return 6
-        
-        cache[1] = 1
-        cache[2] = 6
-        multi[1] = 1
-        multi[2] = 6
-        
-        
-        
-        for i in range(3,n + 1):
-            diff = 2 * multi[i - 1] - multi[i - 2] + 4
-            multi[i] = diff
-            cache[i] = diff * cache[i - 1]
+        #bottom up
+        cache = [0] * n
+        cache[0] = 1
+        slot = 3
+        Mod =  (10 ** 9 + 7)
+        for i in range(2,n + 1):
+            cache[i - 1] = cache[i - 2] * (slot + 1) * slot // 2
+            slot += 2
             
-        return cache[n] % (10 ** 9 + 7)
+        return cache[n - 1] % Mod
             
         
