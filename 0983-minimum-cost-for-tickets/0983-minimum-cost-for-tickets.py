e9c2a5eedf83046f96131a962f07771bc9479c7a
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-16 22:11:46 +0300

    Time: 45 ms (79.99%), Space: 16.7 MB (32.13%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,20 +1,18 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-       
         @cache
-        def dfs(i):
+        def dp(i):
             
-            N = len(days)
-            if i >= N:
+            if i >= len(days):
                 return 0
+            #pay daily
+            day = dp(i + 1) + costs[0]
+            #pay weakly
+            week = dp(bisect.bisect_left(days,days[i] + 7)) + costs[1]
+            #pay monthy
+            month = dp(bisect.bisect_left(days,days[i] + 30)) + costs[2]
             
-            minCost = float("inf")
-            for d,c in zip([1,7,30],costs):
-                j = i
-                while j < N and days[j] < days[i] + d:
-                    j += 1
-                  
-                minCost = min(minCost,dfs(j) + c)
-                
-            return minCost
-        return dfs(0)
+            return min(day,week,month)
+          
+        return dp(0)
+        
