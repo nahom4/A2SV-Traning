168e1f405b224ffce83c9de523e7691f1586b48c
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-16 22:58:09 +0300

    Time: 58 ms (25.36%), Space: 16.3 MB (66.25%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,5 +1,21 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        cache = Counter()
+        left = 0
+        for i in range(1,max(days) + 1):
+            if days[left] == i:
+                left += 1
+                day = cache[i - 1] + costs[0]
+                week = cache[i - 7] + costs[1]
+                month = cache[i - 30] + costs[2]
+                cache[i] = min(day,week,month)
+                
+            else:
+                cache[i] = cache[i - 1]
+                
+        return cache[max(days)]
+            
         @cache
         def dp(i):
             
@@ -15,4 +31,9 @@ class Solution:
             return min(day,week,month)
           
         return dp(0)
+    
+        
+    
+        
+        
         
