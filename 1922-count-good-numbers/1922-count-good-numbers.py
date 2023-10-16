4e7f727e1111381f1e9cc8054702ab0cb1417d85
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-16 10:24:37 +0300

    Time: 34 ms (84.35%), Space: 16.1 MB (96.15%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -5,20 +5,22 @@ class Solution:
         odd = n // 2
         even = odd + n % 2
         
-        def power(num,pw):
-            
-            if pw == 0:
-                return 1
-            if pw == 1:
-                return num
-            
-            if pw % 2 == 0:        
-                return ((power(num,pw // 2) % Mod) ** 2 ) % Mod
+        return ((pow(5,even,Mod)) * (pow(4,odd,Mod))) % Mod
+
         
-            else:
-                return ((power(num,pw // 2) % Mod ) ** 2 % Mod * num ) % Mod
+#         def power(num,pw):
+            
+#             if pw == 0:
+#                 return 1
+#             if pw == 1:
+#                 return num
+            
+#             if pw % 2 == 0:        
+#                 return ((power(num,pw // 2) % Mod) ** 2 ) % Mod
+        
+#             else:
+#                 return ((power(num,pw // 2) % Mod ) ** 2 % Mod * num ) % Mod
         
             
         
-        return ((power(5,even)) % Mod * (power(4,odd)) % Mod) % Mod
         
