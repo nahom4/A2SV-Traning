6bc452ebef5260f789941ecec7f48fa1dd8f2916
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-19 10:48:18 +0300

    Time: 2905 ms (65.02%), Space: 415.5 MB (84.12%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -1,46 +1,40 @@
 class Trie:
     def __init__(self):
-        self.children = [None] * 26
+        self.children = {}
         self.isEnd = False
-        self.value = None
         self.count = 0
         
     def addWord(self,word):
         N = len(word)
         pool = self.children
         for i in range(N):
-            idx = ord(word[i]) - 97
-            if not pool[idx]:
-                pool[idx] = Trie()
-                pool[idx].value = word[i]
+            if not word[i] in pool:
+                pool[word[i]] = Trie()
                 
-            node = pool[idx]
+            node = pool[word[i]]
             node.count += 1
             pool = node.children
             
         node.isEnd = True
         
+        
+                
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         trie = Trie()
-        
         for word in words:
             trie.addWord(word)
-            
+         
         ans = []
-        for word in words: #1000
-            N = len(word)
-            count = 0
-            pool = trie.children
-            for j in range(N):
-                char = word[j]
-                idx = ord(char) - 97
-                if not pool[idx]:
-                    return None
-                node = pool[idx]
-                count += node.count
-                pool = node.children
-
-            ans.append(count)
+        for word in words:
+            ct = 0
+            node = trie
+            for char in word:
+                node = node.children[char]
+                ct += node.count
+                    
+            ans.append(ct)
+            
         return ans
                 
+            
