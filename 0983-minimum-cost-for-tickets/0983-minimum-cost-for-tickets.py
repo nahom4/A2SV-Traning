6657af35687d1b56ed03ecc47af4cfc812f6c966
nahom4 <114163156+nahom4@users.noreply.github.com>
2023-10-24 23:02:26 +0300

    Time: 40 ms (94.70%), Space: 16.3 MB (64.04%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,36 +1,31 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         
-        cache = Counter()
-        left = 0
-        for i in range(1,max(days) + 1):
-            if days[left] == i:
-                left += 1
-                day = cache[i - 1] + costs[0]
-                week = cache[i - 7] + costs[1]
-                month = cache[i - 30] + costs[2]
-                cache[i] = min(day,week,month)
-                
-            else:
-                cache[i] = cache[i - 1]
-                
-        return cache[max(days)]
+        N = len(days)
+        cache = defaultdict(int)
+        for i in range(N - 1,-1,-1):
+            day = cache[bisect.bisect_left(days,days[i] + 1)] + costs[0]
+            week = cache[bisect.bisect_left(days,days[i] + 7)] + costs[1]
+            month = cache[bisect.bisect_left(days,days[i] + 30)] + costs[2]
             
-        @cache
-        def dp(i):
+            cache[i] = min(day,week,month)
+         
+        return cache[0]
+        # @cache
+#         def dp(i):
             
-            if i >= len(days):
-                return 0
-            #pay daily
-            day = dp(i + 1) + costs[0]
-            #pay weakly
-            week = dp(bisect.bisect_left(days,days[i] + 7)) + costs[1]
-            #pay monthy
-            month = dp(bisect.bisect_left(days,days[i] + 30)) + costs[2]
+#             if i >= len(days):
+#                 return 0
+#             #pay daily
+#             day = dp(i + 1) + costs[0]
+#             #pay weakly
+#             week = dp(bisect.bisect_left(days,days[i] + 7)) + costs[1]
+#             #pay monthy
+#             month = dp(bisect.bisect_left(days,days[i] + 30)) + costs[2]
             
-            return min(day,week,month)
+#             return min(day,week,month)
           
-        return dp(0)
+#         return dp(0)
     
         
     
